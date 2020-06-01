f22104a3246dca5ddcf8f7479df172cf2490be6c
Pipe Echeverri <pipe.echeverri.1@gmail.com>
2020-06-01 14:47:02 -0500

    SOCKET CON FECHA, ESTADO Y DESCRIPCION

--- horus_alert.php
+++ horus_alert.php
@@ -1,11 +1,12 @@
 <?php
-setAlerta('Alerta enviada desde Horus6');
+setAlerta($_GET['ds'], $_GET['st'], $_GET['fe']);
 
-function setAlerta($alerta) {
+function setAlerta($ds, $st, $fe) {
     $script = "<script src='js/node_modules/socket.io-client/dist/socket.io.js'></script>";
     $script .= "<script>";
+	$script .= "var messages = [{ ds: '".$ds."', st: '".$st."', fe: '".$fe."' }];"; 
     $script .= "var socket = io.connect( 'http://localhost:25003' );";
-    $script .= "socket.emit( 'sendAlert',  '" . $alerta . "');";
+    $script .= "socket.emit( 'sendAlert',  messages);";
     $script .= "</script>";
     echo $script;
 }
